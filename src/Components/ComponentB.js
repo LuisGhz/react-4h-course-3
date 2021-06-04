90656dbffe5e59ac216a9e6c34621282ba4fa141
Luis Gutiérrez <luisghtz@outlook.com>
2021-06-04 13:15:33 -0500

    Read Context using useContext

--- src/Components/ComponentB.js
+++ src/Components/ComponentB.js
@@ -1,9 +1,15 @@
+import { useContext } from 'react';
 import ComponentC from './ComponentC';
+import { UserContext, ColorContext } from 'App';
 
 const  ComponentB = () => {
+  const user = useContext(UserContext);
+  const color = useContext(ColorContext);
+
   return (
     <div>
       <p>ComponentB</p>
+      <p>name: { user }, color { color }</p>
       <ComponentC />
     </div>
   );
